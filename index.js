340190326585bc06498b338f23ea76733a155724
Prateek Pandey <orion.prateek@gmail.com>
2018-12-19 09:53:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 
 app.post('/getSantaName', function(req, res){
   console.log('Inside get santa name')
-    console.log('Result: ', req.body.result)
+    console.log('Body: ', req.body)
   var intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent"
     , speech = "This is the default speech"
   console.log('Intent: ', intent)
