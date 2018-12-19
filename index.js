0709d6aa12c97aa7c9d33232d6511a5dda1e3b4a
Prateek Pandey <orion.prateek@gmail.com>
2018-12-19 10:33:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,136 @@ app.post('/getSantaName', function(req, res){
     }
     else if(personName === 'Prateek'){
       return res.json({
-            speech: 'Test',
-            displayText: 'Test',
+            speech: 'The secret santa of Prateek is Mansi',
+            displayText: 'he secret santa of Prateek is Mansi',
+            source: 'webhook-santa-app'
+        });
+    }
+     else if(personName === 'Ajay'){
+      return res.json({
+            speech: 'The secret santa of Ajay is Aakash',
+            displayText: 'he secret santa of Ajay is Aakash',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Aakash'){
+      return res.json({
+            speech: 'The secret santa of Aakash is Prateek',
+            displayText: 'he secret santa of Aakash is Prateek',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Mansi'){
+      return res.json({
+            speech: 'The secret santa of Mansi is Ajay',
+            displayText: 'he secret santa of Mansi is Ajay',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Mohan'){
+      return res.json({
+            speech: 'The secret santa of Mohan is Sunil',
+            displayText: 'he secret santa of Mohan is Sunil',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Sunil'){
+      return res.json({
+            speech: 'The secret santa of Sunil is Tarun',
+            displayText: 'he secret santa of Sunil is Tarun',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Tarun'){
+      return res.json({
+            speech: 'The secret santa of Tarun is Hema',
+            displayText: 'he secret santa of Tarun is Hema',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Hema'){
+      return res.json({
+            speech: 'The secret santa of Hema is Sheetal',
+            displayText: 'he secret santa of Hema is Sheetal',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Sheetal'){
+      return res.json({
+            speech: 'The secret santa of Sheetal is Jaswinder',
+            displayText: 'he secret santa of Sheetal is Jaswinder',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Jaswinder'){
+      return res.json({
+            speech: 'The secret santa of Jaswinder is Srini',
+            displayText: 'he secret santa of Jaswinder is Srini',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Srini'){
+      return res.json({
+            speech: 'The secret santa of Srini is Mohan',
+            displayText: 'he secret santa of Srini is Mohan',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Mohan'){
+      return res.json({
+            speech: 'The secret santa of Mohan is Sunil',
+            displayText: 'he secret santa of Mohan is Sunil',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Sanju'){
+      return res.json({
+            speech: 'The secret santa of Sanju is Prathap',
+            displayText: 'he secret santa of Sanju is Prathap',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Prathap'){
+      return res.json({
+            speech: 'The secret santa of Prathap is Sathish',
+            displayText: 'he secret santa of Prathap is Sathish',
+            source: 'webhook-santa-app'
+        });
+    }
+       else if(personName === 'Sathish'){
+      return res.json({
+            speech: 'The secret santa of Sathish is Aswini',
+            displayText: 'he secret santa of Sathish is Aswini',
+            source: 'webhook-santa-app'
+        });
+    }
+       else if(personName === 'Aswini'){
+      return res.json({
+            speech: 'The secret santa of Aswini is Manoj',
+            displayText: 'he secret santa of Aswini is Manoj',
+            source: 'webhook-santa-app'
+        });
+    }
+      else if(personName === 'Manoj'){
+      return res.json({
+            speech: 'The secret santa of Manoj is Akila',
+            displayText: 'he secret santa of Manoj is Akila',
+            source: 'webhook-santa-app'
+        });
+    }
+      
+      else if(personName === 'Akila'){
+      return res.json({
+            speech: 'The secret santa of Akila is Ruban',
+            displayText: 'he secret santa of Akila is Ruban',
+            source: 'webhook-santa-app'
+        });
+    }
+      
+      else if(personName === 'Ruban'){
+      return res.json({
+            speech: 'The secret santa of Ruban is Sanju',
+            displayText: 'he secret santa of Ruban is Sanju',
             source: 'webhook-santa-app'
         });
     }
