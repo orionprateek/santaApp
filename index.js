21605b93634ad29c154f84bfd389a59f4b01e6f4
Prateek Pandey <orion.prateek@gmail.com>
2018-12-19 09:36:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post('/getSantaName', function(req, res){
+  var intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent"
+    , speech = "This is the default speech"
+
+  if(intent === 'noIntent'){
+    return res.json({
+        speech: 'No intent was found',
+        displayText: 'No intent was found',
+        source: 'webhook-santa-app'
+    });
+  }
+  else if(intent === 'santaName'){
+    var personName = req.body.result.parameters.personName ? req.body.result.parameters.personName : 'noPerson'
+    if(personName === 'noPerson'){
+      return res.json({
+          speech: 'Please provide a valid person name',
+          displayText: 'Please provide a valid person name',
+          source: 'webhook-santa-app'
+      });
+    }
+    else if(personName === 'Prateek'){
+      return res.json({
+          speech: 'Please provide a valid person name',
+          displayText: 'Please provide a valid person name',
+          source: 'webhook-santa-app'
+      });
+    }
+    else if(personName === 'Ajay'){
+      return res.json({
+          speech: 'Please provide a valid person name',
+          displayText: 'Please provide a valid person name',
+          source: 'webhook-santa-app'
+      });
+    }
+    else if(personName === 'Mansi'){
+      return res.json({
+          speech: 'Please provide a valid person name',
+          displayText: 'Please provide a valid person name',
+          source: 'webhook-santa-app'
+      });
+    }
+    else if(personName === 'Akash'){
+      return res.json({
+          speech: 'Please provide a valid person name',
+          displayText: 'Please provide a valid person name',
+          source: 'webhook-santa-app'
+      });
+    }
+  }
+})
+
+
+app.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
